175ad37063407a174a4f81fe6b89de884bc27daa
oluwatomisin-sanni <Osanni2068@conestogac.on.ca>
2024-06-14 15:15:46 -0400

    ive created the Positon class

--- Program.cs
+++ Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+
+class Position
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+
+    public Position(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+}
