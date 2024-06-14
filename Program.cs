2e53b5fb5f577ece4247d9f26e2723fd332c5ef2
oluwatomisin-sanni <Osanni2068@conestogac.on.ca>
2024-06-14 18:09:55 -0400

    i have created the class Program which calls on the method holding the game sequence

--- Program.cs
+++ Program.cs
@@ -229,4 +229,13 @@ class Game
 
         AnnounceWinner();
     }
+    class Program
+    {
+        static void Main()
+        {
+            Game game = new Game();
+            game.Start();
+        }
+    }
+
 }
