db704fd1ae4193d8235c5d7de1892bc83ba7c63a
oluwatomisin-sanni <Osanni2068@conestogac.on.ca>
2024-06-14 15:27:18 -0400

    i have created the Player class

--- Program.cs
+++ Program.cs
@@ -12,3 +12,37 @@ class Position
         Y = y;
     }
 }
+class Player
+{
+    public string Name { get; }
+    public Position Position { get; set; }
+    public int GemCount { get; set; }
+    public int Score { get; set; }
+
+    public Player(string name, Position position)
+    {
+        // initializing the gem and score count to 0
+        Name = name;
+        Position = position;
+        GemCount = 0;
+        Score = 0;
+    }
+    public void Move(char direction)
+    {
+        switch (direction)
+        {
+            case 'U':
+                Position.Y -= 1;
+                break;
+            case 'D':
+                Position.Y += 1;
+                break;
+            case 'L':
+                Position.X -= 1;
+                break;
+            case 'R':
+                Position.X += 1;
+                break;
+        }
+    }
+}
